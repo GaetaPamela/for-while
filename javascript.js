aa149781284776d21a98dcd734b4386b3668a261
Gaeta Pamela <odontologia.pamela@gmail.com>
2022-07-01 23:01:27 -0300

    finalizado

---
+++ javascript.js
@@ -0,0 +1,26 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+for( let joao=1;joao<=10; joao++){
+    console.log(joao)
+}
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+ console.log( "------")
+let aline=10
+while(aline>=1){
+    console.log(aline)
+    aline-=1
+
+}
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+console.log("--------")
+for( let namber=1; namber<=100;namber+=2){
+    console.log(namber)
+ }
+
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+console.log("--------")
+for( let numero=0; numero<=100;numero+=2){
+    console.log(numero)
+ }
